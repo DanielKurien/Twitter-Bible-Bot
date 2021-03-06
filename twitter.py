ee4eab4b3f0c41d1bf6216fdff6037801ef453a3
Daniel Kurien <danielmathewkurien@gmail.com>
2021-03-06 14:06:00 -0500

    Added Daily Tweet Function

--- twitter.py
+++ twitter.py
@@ -1,5 +1,7 @@
 import tweepy
-
+import time
+import requests
+import schedule
 
 auth = tweepy.OAuthHandler(ACCESS_TOKEN,
                            ACCESS_TOKEN_SECRET)
@@ -7,3 +9,18 @@ auth.set_access_token(API_KEY,
                       API_KEY_SECRET)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+def dailyTweet():
+    dailyVerseRequest = requests.get(
+        'https://labs.bible.org/api/?passage=random&type=json').json()[0]
+    dailyVerse = f'{dailyVerseRequest["bookname"]} {dailyVerseRequest["chapter"]}:{dailyVerseRequest["verse"]} | {dailyVerseRequest["text"]}'
+    api.update_status(dailyVerse)
+    print("Tweet Tweeted!")
+
+
+schedule.every().day.at("7:00").do(dailyTweet)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
