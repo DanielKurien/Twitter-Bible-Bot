46b7da31cb0597565ab1f5a58617251def31f5ce
Daniel Kurien <danielmathewkurien@gmail.com>
2021-03-06 16:45:02 -0500

    Added comments

--- twitter.py
+++ twitter.py
@@ -1,8 +1,10 @@
+# imports needed for bot
 import tweepy
 import time
 import requests
 import schedule
 
+# setting up authentication with twitter API
 auth = tweepy.OAuthHandler(ACCESS_TOKEN,
                            ACCESS_TOKEN_SECRET)
 auth.set_access_token(API_KEY,
@@ -10,19 +12,24 @@ auth.set_access_token(API_KEY,
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+# function for getting data and making twitter tweet
+
 
 def dailyTweet():
     dailyVerseRequest = requests.get(
-        'https://labs.bible.org/api/?passage=random&type=json').json()[0]
+        'https://labs.bible.org/api/?passage=votd&type=json').json()[0]
     dailyVerse = f'{dailyVerseRequest["bookname"]} {dailyVerseRequest["chapter"]}:{dailyVerseRequest["verse"]} | {dailyVerseRequest["text"]}'
     api.update_status(dailyVerse)
     print("Tweet Tweeted!")
 
 
+# function to follow back users that follow bot
 def follow():
     for follower in tweepy.Cursor(api.followers).items():
         if not follower.following:
             follower.follow()
+
+# function to run functions
 
 
 def bot_functions():
